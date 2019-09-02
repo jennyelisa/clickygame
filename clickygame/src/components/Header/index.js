ce2591737d6687f8c563b117c00c68d60cbf4225
Jenny <jennyelisa07@aol.com>
2019-09-02 18:12:32 -0400

    header styling

--- clickygame/src/components/Header/index.js
+++ clickygame/src/components/Header/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./style.css";
 
 const Header = props => (
-    <div id="header" className="bg-sucess text-light">
-        <h2>CupHead</h2>
+    <div id="header" className="gameHeader">
+        <h1>CupHead</h1>
+        <div className="stats">
         <label>Score: {props.clickedImg}</label>
         <label>High Score: {props.highScore}</label>
         <label>Wins: {props.wins}</label>
         <label>Losses: {props.losses}</label>
+        </div>
     </div>
 );
 
