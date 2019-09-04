08da52f41bc513fcbdc828f992e3db4da4527212
Jenny <jennyelisa07@aol.com>
2019-09-03 21:33:18 -0400

    Updates

---
+++ precache-manifest.44dd475c6ae3af7c6c883dc3a8b741d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b5273625ab5128ca3667f9a50bc6125",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "9c64a3dcf11ad3c7f997",
+    "url": "/clickygame/static/css/main.0a39788e.chunk.css"
+  },
+  {
+    "revision": "997c1737f3d3c7a8e3bf",
+    "url": "/clickygame/static/js/2.c73320c9.chunk.js"
+  },
+  {
+    "revision": "9c64a3dcf11ad3c7f997",
+    "url": "/clickygame/static/js/main.fa6c3f39.chunk.js"
+  },
+  {
+    "revision": "0767f4cbf998e7d82d82",
+    "url": "/clickygame/static/js/runtime~main.832da4da.js"
+  }
+]);
